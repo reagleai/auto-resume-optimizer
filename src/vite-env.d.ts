8d772f0a7069288b14ca9fe82a7936c27f49cdae
Ajay Sharma <reagleai@gmail.com>
2026-04-16 02:42:39 +0530

    Update Supabase env variable keys to match Vercel integration

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_SUPABASE_URL: string
-  readonly VITE_SUPABASE_ANON_KEY: string
+  readonly VITE_SUPABASE_SUPABASE_URL: string
+  readonly VITE_SUPABASE_SUPABASE_ANON_KEY: string
   readonly VITE_APP_PASSWORD: string
 }
 
